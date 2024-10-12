d527dd2b395d6173378fc4a2eb7d81a00a38850e
Officer_Choi <me940728@naver.com>
2024-10-12 16:15:42 +0900

    [UPDATE] 수정요

--- LAB_DEMO/start.sh
+++ LAB_DEMO/start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# 반복 횟수 정의 drone_info 에 있는 전체 드론노드 수와 맟게 반복해야 함
+# 반복 횟수 정의
 REPEAT_COUNT=7
-CONFIG_FILE="drone_info.yaml"
+DRONE_CONFIG_FILE="drone_info.yaml"
+BANDWIDTH_FILE="bandwidth_info.yaml"  # 대역폭 정보를 담은 파일
 
-# 반복 횟수만큼 Python 스크립트를 비동기로 실행(순서 보장 x 먼저 생성된 p가 먼저 로그 찍힘)
+# 반복 횟수만큼 Python 스크립트를 비동기로 실행(순서 보장 x)
 for i in $(seq 0 $(($REPEAT_COUNT-1))); do
-  python drone.py $i $CONFIG_FILE &
+  python drone.py $i $DRONE_CONFIG_FILE $BANDWIDTH_FILE &
 done
 
 wait  # 모든 백그라운드 프로세스가 종료될 때까지 대기
